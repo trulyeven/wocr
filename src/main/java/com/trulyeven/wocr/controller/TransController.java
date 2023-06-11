5b997bb36609806e922d8a4a75ad145989473384
trulyeven <trulyeven@gmail.com>
2023-06-11 22:30:03 +0900

    selenium 실행 완성

--- src/main/java/com/trulyeven/wocr/controller/TransController.java
+++ src/main/java/com/trulyeven/wocr/controller/TransController.java
@@ -1,31 +1,42 @@
 package com.trulyeven.wocr.controller;
 
-import java.io.File;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import net.sourceforge.tess4j.*;
+import com.trulyeven.wocr.service.TransService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class TransController {
 
-	@GetMapping("trans")
-	public String trans() {
+	@Autowired 
+	TransService service;
+	
+	
+	@GetMapping("/trans")
+	public String trans(Model model) {
 		
-		File imageFile = new File("C:/Users/g_yun/Desktop/example.png"); // 이미지파일 경로
-		ITesseract instance = new Tesseract();
-		instance.setLanguage("kor+eng");  // OCR 한글 영어 인식
-        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
-        instance.setDatapath("C:/Program Files/Tesseract-OCR/tessdata"); // tessdata directory 경로
-
-        try {
-            String result = instance.doOCR(imageFile);
-            System.out.println(result);
-        } catch (TesseractException e) {
-            System.err.println(e.getMessage());
-        }
+		String result = service.tessOCR();  // OCR 실행
+		model.addAttribute("result", result);
+	
         
+		return "trans";
+	}
+	
+	@PostMapping("/trans")
+	public String transPost(String url, Model model) {
+		String result = service.tessOCR();  // OCR 실행
+		
+		model.addAttribute("url", url);
+		model.addAttribute("result", result);
+	
+		service.screenshot(url);
+	    
 		return "trans";
 	}
 }
--- src/main/java/com/trulyeven/wocr/service/TransService.java
+++ src/main/java/com/trulyeven/wocr/service/TransService.java
@@ -1,5 +1,47 @@
 package com.trulyeven.wocr.service;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.springframework.stereotype.Service;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+@Service
 public class TransService {
 
+	/**
+	 * 
+	 * @return
+	 */
+	public String tessOCR() {
+		File imageFile = new File("C:/Users/g_yun/Desktop/example.png"); // 이미지파일 경로
+		ITesseract instance = new Tesseract();
+		instance.setLanguage("kor+eng");  // OCR 한글 영어 인식
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+        instance.setDatapath("C:/Program Files/Tesseract-OCR/tessdata"); // tessdata directory 경로
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+            return result;
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+		return null;
+	}
+    
+	/**
+	 */
+	public void screenshot(String url) {
+	    
+		System.getProperty("webdriver.edge.driver", "C:\\worktool\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.get(url);
+		driver.quit();
+		
+	}
 }
