65daab8247a5238b88737b43a31cf6d794a707e8
trulyeven <trulyeven@gmail.com>
2023-06-21 20:38:21 +0900

    youtube url 유효성 검사

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,14 +1,26 @@
 /**
  * 
- */
+*/
 
- // 유효한 YouTube 비디오 ID 패턴
-var youtubeIdPattern = /^[A-Za-z0-9_-]{11}$/;
+// input 유튜브 주소 유효성 검사
+document.addEventListener('DOMContentLoaded', function() {
+    const input = document.getElementById('url');
+    const submitButton = document.getElementById('submit');
+    const inputalert = document.getElementById('inputalert');
 
-// 입력 필드 값 변경 시 유효성 검사
-document.getElementById('videoId').addEventListener('input', function() {
-    var videoId = this.value;
-    var validVideoId = youtubeIdPattern.test(videoId);
-    this.setAttribute('disabled', !validVideoId);
-    document.querySelector('button[type="submit"]').disabled = !validVideoId;
-});
+    input.addEventListener('input', function() {
+      const inputValue = input.value;
+      const pattern = /(?:watch\?v=|\/videos\/|embed\/|youtu.be\/|\/v\/|\/e\/|watch\?v%3D|watch\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)([^#\&\?\n]*)/;
+      const matchResult = inputValue.match(pattern);
+      const videoCode = matchResult ? matchResult[1] : '';
+  
+      if (videoCode === null || videoCode === '') {
+        submitButton.disabled = true;
+        inputalert.hidden = false;
+    } else {
+        submitButton.disabled = false;
+        inputalert.hidden = true;
+      }
+    });
+  });
+  
